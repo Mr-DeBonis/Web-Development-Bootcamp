cc01d940b40d71b0017878dd41242904d08843eb
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-09-11 23:57:55 -0300

    Create a new pattern

--- 15_Simon-game/Simon Game Challenge Starting Files/game.js
+++ 15_Simon-game/Simon Game Challenge Starting Files/game.js
@@ -1,1 +1,15 @@
-alert("Hello");
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    return randomNumber;
+}
+
+var randomChosenColour = buttonColours[nextSequence()];
+
+console.log(randomChosenColour);
+
+gamePattern.push(randomChosenColour);
+
+myButton = $(randomChosenColour);
