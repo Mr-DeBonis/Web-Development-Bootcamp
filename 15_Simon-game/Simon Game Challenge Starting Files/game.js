cdd5612d33b70c8dd3d37430540eea980c2e50ed
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-09-21 01:53:03 -0300

    Check the user's answer against the game sequence

--- 15_Simon-game/Simon Game Challenge Starting Files/game.js
+++ 15_Simon-game/Simon Game Challenge Starting Files/game.js
@@ -4,21 +4,24 @@ var gamePattern = [];
 var userClickedPattern = [];
 
 function nextSequence() {
-    $('h1').text("Level " + level);
-    level++;
+    userClickedPattern = [];
     
+    $('h1').text("Level " + level);
+
     var randomNumber = Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
 
-    //console.log(randomChosenColour);
 
     gamePattern.push(randomChosenColour);
     $('#' + randomChosenColour).fadeOut(200).fadeIn(200);
-    // Play button sound
+
     playSound(randomChosenColour);
+
+    level++;
 }
 
 function playSound(name){
+    // Play button sound
     var sound = new Audio('./sounds/' + name + '.mp3');
     sound.play();
 }
@@ -35,6 +38,21 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function checkAnswer(currentLevel){
+    console.log(userClickedPattern);
+    console.log(gamePattern);
+    var equals = true;
+    
+    if (userClickedPattern[currentLevel] !== gamePattern[currentLevel]){
+        equals = false;
+    } 
+
+    if(equals){
+        console.log('Success!');
+    } else {
+        console.log('Wrong');
+    }
+}
 
 //////////////////////////////////////////////////
 var level = 0;
@@ -49,5 +67,14 @@ $(".btn").click(function() {
     userClickedPattern.push(userChosenColour);
     playSound(userChosenColour);
     animatePress(userChosenColour);
-    
+
+    checkAnswer(userClickedPattern.length - 1);
+
+    if(userClickedPattern.length === gamePattern.length){
+        console.log('hola');
+        setTimeout(() => {
+            nextSequence();
+        }, 1000);
+    }
 });
+
