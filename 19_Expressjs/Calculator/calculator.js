ed7cb9cc207fd167d5631130c99352685c18261f
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-12-01 17:47:29 -0300

    Add Calcualtor

---
+++ 19_Expressjs/Calculator/calculator.js
@@ -0,0 +1,26 @@
+/** @format */
+
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(3000, function () {
+    console.log("Starting server on port 3000");
+});
+
+app.get("/", function (request, response) {
+    response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (request, response) {
+    num1 = Number(request.body.num1);
+    num2 = Number(request.body.num2);
+
+    var result = num1 + num2;
+
+    response.send("Your result is " + result);
+});
