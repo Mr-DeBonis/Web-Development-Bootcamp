4a85f1100d55ec2d723eaf6d955f433044e3438c
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-12-05 00:15:26 -0300

    Update openweathermap API key

---
+++ 20_API/WeatherProject/README.md
@@ -0,0 +1,3 @@
+# Configuration
+1. Create an account at [openweathermap](https://openweathermap.org)
+2. Save an api key inside a file called `api_key.txt`
--- 20_API/WeatherProject/app.js
+++ 20_API/WeatherProject/app.js
@@ -5,9 +5,10 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
@@ -17,24 +18,43 @@ app.listen(3000, function () {
     console.log("Server is running on port 3000.");
 });
 
-app.post("/", function(req, res){
+app.post("/", function (req, res) {
     const query = req.body.cityName;
-    const api_key= "d7efd29dddf7581ec4b228c2c874a14e";
-    const units = "metric"
-    
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + api_key + "&units=" + units + "&lang=es";
-    
+    const units = "metric";
+
+    // Read openweathermap API key
+    const data = fs.readFileSync(__dirname + "/api_key.txt", "utf8");
+
+    const api_key = data.toString();
+
+    const url =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        query +
+        "&appid=" +
+        api_key +
+        "&units=" +
+        units +
+        "&lang=es";
+
     https.get(url, function (response) {
         console.log(response.statusCode);
         response.on("data", function (data) {
             var weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
-            const imageURL = "http://openweathermap.org/img/wn/" + weatherData.weather[0].icon + "@2x.png"
-
+            const imageURL =
+                "http://openweathermap.org/img/wn/" +
+                weatherData.weather[0].icon +
+                "@2x.png";
 
             res.write("<p>El tiempo es " + weatherDescription + "</p>");
-            res.write("<h1>La temperatura en " + query + " es " + temp + " grados Celsius.</h1>");
+            res.write(
+                "<h1>La temperatura en " +
+                    query +
+                    " es " +
+                    temp +
+                    " grados Celsius.</h1>"
+            );
             res.write('<img src="' + imageURL + '">');
             res.send();
         });
