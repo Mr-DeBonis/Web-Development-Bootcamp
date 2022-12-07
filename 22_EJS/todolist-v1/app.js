c7cc5a9ccf7fdb4e3be853ff65d5bdcc7a992656
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-12-07 19:13:19 -0300

    Start todolist-v1 project

---
+++ 22_EJS/todolist-v1/app.js
@@ -0,0 +1,26 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+
+    if (currentDay === 6 || currentDay === 0){
+        res.write("<h1>Yay It's the weekend!</h1>");
+    } else{
+        res.write("<p>It's not the weekend</p>");
+        res.write("<h1>Boo! I have to work!</h1>");
+    }
+    res.send();
+})
+
+app.listen(3000, function () {
+    console.log("Server started on port 3000.");
+})
