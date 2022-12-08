16bcc0f46c4c28547978af8f9f8dee0d626517b6
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-12-07 21:26:57 -0300

    Add css  styles

--- 22_EJS/todolist-v1/app.js
+++ 22_EJS/todolist-v1/app.js
@@ -5,18 +5,35 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
 
-app.get("/", function (req, res) {
-    var today = new Date();
-    var day = days[today.getDay()];
-
-    res.render('list', {kindOfDay : day});
-})
+var items = [];
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 })
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    const options = {weekday : 'long',
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric'};
+    
+    var day = today.toLocaleDateString('en-US', options)
+
+
+
+    res.render('list', {kindOfDay : day, nextItems :items});
+})
+
+app.post("/",function (req,res) {
+    items.push(req.body.nextItem);
+    res.redirect("/");
+})
+
+
+
+
