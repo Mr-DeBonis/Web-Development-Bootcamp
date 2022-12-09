a669509aad9c2d0f204bff40e8f063f80902af73
Mr-DeBonis <ignacio.debonis7@gmail.com>
2022-12-08 22:32:15 -0300

    Add header, footer EJS layouts and about page

--- 22_EJS/todolist-v1/app.js
+++ 22_EJS/todolist-v1/app.js
@@ -1,3 +1,5 @@
+/** @format */
+
 //jshint esversion:6
 
 const express = require("express");
@@ -5,35 +7,51 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-var items = [];
+let items = [];
+let workItems = [];
 
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
-})
+});
 
 app.get("/", function (req, res) {
-    var today = new Date();
-    const options = {weekday : 'long',
-                    year: 'numeric',
-                    month: 'long',
-                    day: 'numeric'};
-    
-    var day = today.toLocaleDateString('en-US', options)
+    let today = new Date();
+    const options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    res.render("list", { listTitle: day, nextItems: items });
+});
+
+app.post("/", function (req, res) {
+    if (req.body.list == "Work") {
+        workItems.push(req.body.nextItem);
+        res.redirect("/work");
+    } else {
+        items.push(req.body.nextItem);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function (req, res) {
+    let header = "Work List";
+
+    res.render = res.render("list", {
+        listTitle: header,
+        nextItems: workItems,
+    });
+});
 
 
-
-    res.render('list', {kindOfDay : day, nextItems :items});
-})
-
-app.post("/",function (req,res) {
-    items.push(req.body.nextItem);
-    res.redirect("/");
-})
-
-
-
-
+app.get("/about", function (req, res) {
+    res.render("about");
+});
