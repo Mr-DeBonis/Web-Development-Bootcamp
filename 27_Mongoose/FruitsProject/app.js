78b263fb53402a30fd82186d7d1ddeacf746ff4d
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-03-28 15:04:32 -0300

    Add many fruits

--- 27_Mongoose/FruitsProject/app.js
+++ 27_Mongoose/FruitsProject/app.js
@@ -2,7 +2,7 @@
 
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://0.0.0.0:27017/fruitsDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://0.0.0.0:27017/fruitsDB", { useNewUrlParser: true });
 
 // Create fruits schema
 const fruitSchema = new mongoose.Schema({
@@ -19,71 +19,52 @@ const fruit = new Fruit({
     rating: 7,
     review: "Pretty solid as fruit."
 });
-console.log("Hola");
-fruit.save();
-console.log("Hola");
 
-/* 
-// Replace the placeholder with your Atlas connection string
-const uri = "mongodb://0.0.0.0:27017/";
+//fruit.save();
+//console.log("Fruit saved to DB");
 
-//Database Name
-const dbName = "fruitsDB";
+////////////////////////////////////////////////////7
+// Create person schema
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+});
 
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+// Create model (collection name is 'Person')
+const Person = mongoose.model("Person", personSchema);
 
-    // Send a ping to confirm a successful connection
-    await client.db(dbName).command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+const person = new Person({
+    name: "John",
+    age: 37
+});
+// person.save();
+// console.log("Person saved to DB");
 
-    // Insert documents to collection
-    const collectionName = "fruits";
+////////////////////////////////////////////////////7
+// Insert several fruits at once
 
-    const db = client.db(dbName);
-    const collection = db.collection(collectionName);
-    try {
-      const docs = [
-        {
-          name: "Apple",
-          score: 8,
-          review: "Great fruit"
-        },
-        {
-          name: "Orange",
-          score: 6,
-          review: "Kinda sour"
-        },
-        {
-          name: "Banana",
-          score: 9,
-          review: "Great stuff!"
-        }
-      ];
+const kiwi = new Fruit({
+    name: "Kiwi",
+    score: 10,
+    review: "The best fruit!"
+});
 
-      const insertManyresult = await collection.insertMany(docs);
-      let ids = insertManyresult.insertedIds;
-      console.log(`${insertManyresult.insertedCount} documents were inserted.`);
-      for (let id of Object.values(ids)) {
-        console.log(`Inserted a document with id ${id}`);
-      }
+const orange = new Fruit({
+    name: "Orange",
+    score: 4,
+    review: "Too sour for me"
+});
 
-    } catch (e) {
-      console.log(`A MongoBulkWriteException occurred, but there are successfully processed documents.`);
-      let ids = e.result.result.insertedIds;
-      for (let id of Object.values(ids)) {
-        console.log(`Processed a document with id ${id._id}`);
-      }
-      console.log(`Number of documents inserted: ${e.result.result.nInserted}`);
-    }
+const banana = new Fruit({
+    name: "Banana",
+    score: 3,
+    review: "Weird texture"
+});
 
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir); 
-
-*/
+Fruit.insertMany([kiwi, orange, banana])
+    .then(function () {
+        console.log("Succesfully saved all the fruits to fruitsDB!");
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
