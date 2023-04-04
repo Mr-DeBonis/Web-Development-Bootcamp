58838eb49e1452d38832df20ff3260682d35a2b7
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-04-04 14:24:17 -0400

    Add project hosted on Heroku

---
+++ 29_Deploy_To_Do/README.md
@@ -0,0 +1,7 @@
+This project was initially deployed to Heroku. By nomvember 2022, Heroku is no longer free, so this project will not be hosted on Heroku.
+
+Although it is presented as it is, including working configuration to deplyo with Heroku.
+
+# dotenv
+This node package helps hidde important information, such as login credentials or api keys. It is used in current project.
+
---
+++ 29_Deploy_To_Do/todolist-v2/app.js
@@ -0,0 +1,158 @@
+/** @format */
+
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Connect to mongoDB with hidden credentials
+require("dotenv").config(); 
+const srvr = process.env.N1_KEY; 
+const srvrCred = process.env.N1_SECRET; 
+const mongoDB = "mongodb+srv://" + srvr + ":" + srvrCred + "@cluster0.dclufwn.mongodb.net/todolistDB";
+
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+// Create an Item schema
+const itemsSchema = new mongoose.Schema({
+    name: String
+});
+
+// Create an item model
+const Item = mongoose.model("Item", itemsSchema);
+
+// 3 default items
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log("Server started.");
+});
+
+app.get("/", function (req, res) {
+    Item.find({})
+        .then(function (foundItems) {
+            if (foundItems == 0) {
+                Item.insertMany(defaultItems)
+                    .then(function () {
+                        console.log("Succesfully saved default items to DB.");
+                    })
+                    .catch(function (error, docs) {
+                        console.log(error);
+                    });
+
+                res.redirect("/");
+            } else {
+                res.render("list", { listTitle: "Today", nextItems: foundItems });
+            }
+        });
+});
+
+app.get("/:customListName", function (req, res) {
+    const customListName = _.capitalize(req.params.customListName);
+    if (customListName == "Favicon.ico") return;
+    console.log("Entering " + customListName);
+
+    List.findOne({ name: customListName })
+        .then(function (foundLists) {
+            if (!foundLists) {
+                const list = new List({
+                    name: customListName,
+                    items: defaultItems
+                });
+
+                list.save();
+                console.log("Succesfully saved default items to DB.");
+
+                res.redirect("/" + customListName);
+            } else {
+                res.render("list", { listTitle: foundLists.name, nextItems: foundLists.items });
+            }
+        });
+
+});
+
+app.post("/", function (req, res) {
+    const itemName = req.body.nextItem;
+    const listName = _.capitalize(req.body.list);
+
+    console.log("Saving to " + listName);
+
+    const item = new Item({
+        name: itemName
+    });
+
+    if (listName === "Today") {
+        item.save();
+        console.log("Item saved to list")
+        res.redirect("/");
+        console.log("Today");
+    } else {
+        List.findOne({ name: listName })
+            .then(function (foundList) {
+                foundList.items.push(item);
+
+                foundList.save();
+
+                console.log("Item saved to list")
+
+                res.redirect("/" + listName);
+            });
+    };
+});
+
+app.post("/delete", function (req, res) {
+    const checkedItemId = req.body.checkbox;
+    const listName = _.capitalize(req.body.listName);
+
+    if (listName === "Today") {
+        Item.findByIdAndRemove(checkedItemId)
+            .then(function () {
+                console.log("Succesfully deleted checked item");
+                res.redirect("/");
+            })
+            .catch(function (err, items) {
+                console.log(err);
+            });
+    } else {
+        List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } })
+            .then(function (foundList) {
+                console.log("Succesfully deleted checked item");
+                res.redirect("/" + listName);
+            });
+    };
+
+});
+
+
+
+app.get("/about", function (req, res) {
+    res.render("about");
+});
