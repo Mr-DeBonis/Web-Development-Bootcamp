4abbefdfa3e743a6a31cc0254b1c3c28e5700391
Mr-DeBonis <ignacio.debonis7@gmail.com>
2023-04-19 07:09:46 -0400

    Add Level 1 - Username & password

--- 32_Authentication_Security/Secrets-StartingCode/app.js
+++ 32_Authentication_Security/Secrets-StartingCode/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,18 +12,60 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+mongoose.connect("mongodb://0.0.0.0:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 })
 
 app.get("/", function (req, res) {
-  res.render("home");
+    res.render("home");
 });
 
 app.get("/login", function (req, res) {
-  res.render("login");
+    res.render("login");
 });
 
 app.get("/register", function (req, res) {
-  res.render("register");
+    res.render("register");
+});
+
+app.post("/register", function (req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save()
+        .then(function () {
+            res.render("secrets");
+        })
+        .catch(error => {
+            console.log(error);
+        });
+
+});
+
+app.post("/login", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({ email: username })
+        .then(foundUser => {
+            if (foundUser.password === password) {
+
+                res.render("secrets");
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
